5597fccc313a915bdf85abed5827e61786d18c09
G00414253@atu.ie <g00414253@atu.ie>
2024-11-27 15:10:48 +0000

    Created membership microservice

--- src/main/java/ie/atu/membershipdemo/MembershipController.java
+++ src/main/java/ie/atu/membershipdemo/MembershipController.java
@@ -1,5 +1,6 @@
 package ie.atu.membershipdemo;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,8 @@ import java.time.Period;
 @RequestMapping("/membership")
 @RestController
 public class MembershipController {
-    private String addMembership(@RequestBody Person person){
-        return "New MemberShip" + person;
+    @PostMapping
+    private String NewMembership(@RequestBody Person person){
+        return "New member " + person ;
     }
 }
